1606f1392c9a93c15efc5e5dacfe46d75114ee61
George Ivanov <me@geovi.dev>
2022-04-17 00:55:27 +0300

    Fixed logo responsivity

--- src/views/pages/LandingPage.tsx
+++ src/views/pages/LandingPage.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import Page from '../../components/styled/Page';
 import Logo from '../../components/Logo';
-import {Dimensions, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import PageTypes from '../../types/PageTypes';
 import {useResponsiveViewConditional} from '../../hooks/useResponsiveViewConditional';
-
-const {width} = Dimensions.get('window');
 
 /***
  * No need of the index for this one but using the types for structure
  * @constructor
  */
 function LandingPage({}: PageTypes) {
-  const responsiveWidth = useResponsiveViewConditional([
-    0.4 * width,
-    0.6 * width,
-  ]);
+  const responsiveWidth = useResponsiveViewConditional(['40%', '60%']);
 
   return (
     <Page style={styles.page}>
